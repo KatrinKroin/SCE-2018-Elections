4e2f04726d1630bdad2646460b16a25664691253
Shai Hod <silvering@gmail.com>
2018-01-09 21:28:21 +0200

    cloudshell file update

--- cloudshell_api.py
+++ cloudshell_api.py
@@ -1,14 +1,15 @@
 from cloudshell.api.cloudshell_api import CloudShellAPISession
+import sys
 
-server_ip = '172.31.22.239' ## This is the internal IP of our CloudShell in AWS
-reservation_id = '0ba4f671-50f4-420a-ba34-07bfd4b73c87'
+server_ip = sys.argv[1] ##'172.31.22.239'  This is the internal IP of our CloudShell in AWS
+reservation_id = sys.argv[2] ##'0ba4f671-50f4-420a-ba34-07bfd4b73c87'
 DEPLOYED_APP_MODEL = 'Generic Deployed App'
 
 session = CloudShellAPISession(server_ip,
                                sys.argv[3],
                                sys.argv[4],
                                sys.argv[5])  ##make sure
-										  ##to pass these credentials from jenkins and don't store them in GitHub!!
+										    ##to pass these credentials from jenkins and don't store them in GitHub!!
 
 resources = session.GetReservationDetails(reservation_id).ReservationDescription.Resources
 my_resource = [resource for resource in resources if resource.ResourceModelName == DEPLOYED_APP_MODEL]
