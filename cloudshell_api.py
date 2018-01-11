374d79867888cc091e16e4976d942972b85670f5
Shai Hod <silvering@gmail.com>
2018-01-11 14:41:31 +0200

    update

--- cloudshell_api.py
+++ cloudshell_api.py
@@ -1,6 +1,5 @@
 from cloudshell.api.cloudshell_api import CloudShellAPISession
 import sys
-import logging
 
 server_ip = sys.argv[1]
 reservation_id = sys.argv[2]
@@ -10,12 +9,15 @@ session = CloudShellAPISession(server_ip,
                                sys.argv[3],
                                sys.argv[4],
                                sys.argv[5])
-logging.warning(session)
+
 resources = session.GetReservationDetails(reservation_id).ReservationDescription.Resources
-my_resource = [resource for resource in resources
-               if resource.ResourceModelName == DEPLOYED_APP_MODEL]
 
-if len(my_resource) > 1:
-    raise Exception('There are more then one app in the sandbox')
 
-print (my_resource[0].FullAddress)
+resource_attributes = session.GetResourceDetails(resources).ResourceAttributes
+public_ip = None
+for att in resource_attributes:
+    if att.Name == 'Public IP':
+        public_ip = att.Value
+        break
+
+print (public_ip)
