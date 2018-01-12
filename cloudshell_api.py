8c3eec591b9f9e3bdaa40323fcc87ddcbb1f8118
Shai Hod <silvering@gmail.com>
2018-01-12 13:03:53 +0200

    update

--- cloudshell_api.py
+++ cloudshell_api.py
@@ -5,19 +5,25 @@ server_ip = sys.argv[1]
 reservation_id = sys.argv[2]
 DEPLOYED_APP_MODEL = 'Generic Deployed App'
 
-session = CloudShellAPISession(server_ip,
-                               sys.argv[3],
-                               sys.argv[4],
-                               sys.argv[5])
+session = CloudShellAPISession(server_ip, sys.argv[3], sys.argv[4], sys.argv[5])
 
 resources = session.GetReservationDetails(reservation_id).ReservationDescription.Resources
 
+my_resource = [resource for resource in resources
+               if resource.ResourceModelName == DEPLOYED_APP_MODEL]
 
-resource_attributes = session.GetResourceDetails(resources).ResourceAttributes
+if len(my_resource) > 1:
+    raise Exception('There are more then one app in the sandbox')
+
+if len(my_resource) == 0:
+    raise Exception('There are no deployed application in the sandbox')
+
+resource_attributes = session.GetResourceDetails(my_resource[0].Name).ResourceAttributes
+
 public_ip = None
 for att in resource_attributes:
     if att.Name == 'Public IP':
         public_ip = att.Value
         break
 
-print (public_ip)
+print (my_resource[0].FullAddress)
