ada47b13ffa5c18c14aab7b4850b54074fe4cb9b
Shai Hod <silvering@gmail.com>
2018-01-13 23:40:55 +0200

    updates

--- cloudshell_api.py
+++ cloudshell_api.py
@@ -1,20 +1,16 @@
-from cloudshell.api.cloudshell_api import CloudShellAPISession
 import sys
 
-server_ip = sys.argv[1] ##This is the internal IP of our CloudShell in AWS
-reservation_id = sys.argv[2]
-DEPLOYED_APP_MODEL = 'Generic Deployed App'
+from cloudshell.api.cloudshell_api import CloudShellAPISession
 
-session = CloudShellAPISession(server_ip,
-                               sys.argv[3],
-                               sys.argv[4],
-                               sys.argv[5])  ##make sure
-##to pass these credentials from jenkins and don't store them in GitHub!!
+server_ip = sys.argv[1]
+reservation_id = sys.argv[2]
+DEPLOYED_APP_MODEL = 'Generic App Model'
+
+session = CloudShellAPISession(server_ip, sys.argv[3], sys.argv[4], sys.argv[5])
 
 resources = session.GetReservationDetails(reservation_id).ReservationDescription.Resources
 
-my_resource = [resource for resource in resources
-               if resource.ResourceModelName == DEPLOYED_APP_MODEL]
+my_resource = [resource for resource in resources if resource.ResourceModelName == DEPLOYED_APP_MODEL]
 
 if len(my_resource) > 1:
     raise Exception('There are more then one app in the sandbox')
@@ -22,11 +18,9 @@ if len(my_resource) > 1:
 if len(my_resource) == 0:
     raise Exception('There are no deployed application in the sandbox')
 
-resource_attributes = session.GetResourceDetails(my_resource[0].Name).ResourceAttributes
-public_ip = None
-for att in resource_attributes:
-    if att.Name == 'Public IP':
-        public_ip = att.Value  ## for getting the public ip
-        break
+resource_att = session.GetResourceDetails(my_resource[0].Name).ResourceAttributes
 
-print (my_resource[0].FullAddress)
+for item in resource_att:
+    if item.Name == 'Public IP':
+        sys.stdout.write(item.Value)
+        break
